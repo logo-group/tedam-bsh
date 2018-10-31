e492c6fda729172598124bbbee5cac624add710c
Can Berk İzgi <canberk_izgi@hotmail.com>
2018-10-31 13:28:35 +0300

    Deleted lib folder

--- lib/updateMaven4LbsApplicationTester.bat
+++
@@ -1,2 +0,0 @@
-mvn install:install-file -Dfile=LbsApplicationTester.jar -DgroupId=com.lbs.tedam.testing -DartifactId=LbsApplicationTester -Dversion=1.0 -Dpackaging=jar
-pause
--- lib/updateMaven4LbsComponents.bat
+++
@@ -1,2 +0,0 @@
-mvn install:install-file -Dfile=LbsComponents.jar -DgroupId=com.lbs.tedam.testing -DartifactId=LbsComponents -Dversion=1.0 -Dpackaging=jar
-pause
